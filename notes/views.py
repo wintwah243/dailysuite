7f0e912c9044828ab9a6961e7e30985e5797c40f
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2026-02-01 21:26:18 +0630

    note CRUD

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Note
+from .forms import NoteForm
 
-# Create your views here.
+
+def note_list(request):
+    notes = Note.objects.all().order_by('-created_at')
+    return render(request, 'notes/note_list.html', {'notes': notes})
+
+def note_create(request):
+    form = NoteForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('note_list')
+    return render(request, 'notes/note_form.html', {'form': form})
+
+def note_update(request, id):
+    note = get_object_or_404(Note, id=id)
+    form = NoteForm(request.POST or None, instance=note)
+    if form.is_valid():
+        form.save()
+        return redirect('note_list')
+    return render(request, 'notes/note_form.html', {'form': form})
+
+def note_delete(request, id):
+    note = get_object_or_404(Note, id=id)
+    if request.method == 'POST':
+        note.delete()
+        return redirect('note_list')
+    return render(request, 'notes/note_confirm_delete.html', {'note': note})
