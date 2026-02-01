b743277db3cc4a4b0fb4990392241b9d2d48b873
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2026-02-01 11:02:13 +0630

    model register in admin(Budget)

--- budget/admin.py
+++ budget/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from budget.models import Category, Expense, Income
+
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Expense)
+admin.site.register(Income)
