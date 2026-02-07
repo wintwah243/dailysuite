3923ec19fc384c49ec6060ad3c835d2aad4a97b6
yinnyeinhtwe <kangjiinee@gmail.com>
2026-02-07 12:59:26 +0630

    Update todo list design

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,6 @@
+from asyncio import tasks
+
+from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import *
 from .models import *
@@ -38,13 +41,36 @@ def todo(request):
 
     # Task statistics
     completed_count = tasks.filter(is_completed=True).count()
-    pending_count = tasks.filter(is_completed=False).count()
+    pending_count = tasks.filter(is_completed=False).filter(Q(due_date__gte=today) | Q(due_date__isnull=True)).count()
 
     # Calculate overdue count
     overdue_count = 0
     for task in tasks.filter(is_completed=False):
         if task.due_date and task.due_date < today:
             overdue_count += 1
+
+    # Pending,Done and Overdue Task
+    inprogress = tasks.filter(is_completed=False) .filter(Q(due_date__gte=today) | Q(due_date__isnull=True))#Inprogress = Pending
+    done = tasks.filter(is_completed=True)
+    overdue = tasks.filter(is_completed=False, due_date__lt=today)
+
+
+    #For Progress task percent and done task percent
+    total_tasks = tasks.count()
+
+    if total_tasks > 0:
+        done_percent = (completed_count / total_tasks) * 100
+        inprogress_percent = (pending_count / total_tasks) * 100
+        overdue_percent = (overdue_count / total_tasks) * 100
+    else:
+        done_percent = 0
+        inprogress_percent = 0
+        overdue_percent = 0
+
+    # #Search Bar
+    q = request.GET.get("q", "")
+    if q:
+        tasks = tasks.filter(name__icontains=q)
 
     context = {
         'tasks': tasks,
@@ -61,6 +87,20 @@ def todo(request):
         'completed_count': completed_count,
         'pending_count': pending_count,
         'overdue_count': overdue_count,
+
+        # Task Status
+        'inprogress': inprogress,
+        'done': done,
+        'overdue': overdue,
+
+        #Task Percent
+        'done_percent': round(done_percent),
+        'inprogress_percent': round(inprogress_percent),
+        'overdue_percent': round(overdue_percent),
+
+
+        #Search Bar
+        'query': q,
     }
 
     return render(request, 'todos/todohome.html', context)
@@ -94,3 +134,13 @@ def toggle_task(request, id):
     task.is_completed = not task.is_completed
     task.save()
     return redirect('todolist')
+
+# def status(request):
+#     inprogress = Task.objects.filter(is_completed=False)
+#     done = Task.objects.filter(is_completed=True)
+#
+#     return render(request, "todos/todohome.html", {
+#         "inprogress": inprogress,
+#         "done": done,
+#     })
+
